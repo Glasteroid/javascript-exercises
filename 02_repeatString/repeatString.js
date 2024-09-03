1554212f6776543f0f215b870ffc23d00fab06ea
Glasteroid <kennethangeles880@yahoo.com>
2024-09-03 12:31:29 -0700

    modified hella files

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, numTimes) {
+    
+    let repeatedString = "";
 
+    if (numTimes < 0) {
+        return 'ERROR';
+    }
+    
+    for (let i = 0; i < numTimes; ++i) {
+        repeatedString += word;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray) {
+    
+    let currentTarget = 0;
+
+    for (let i = 1; i < arguments.length; i++) {
+        currentTarget = arguments[i];
+
+        for (let j = 0; j < givenArray.length; j++) {
+            if (currentTarget === givenArray[j]) {
+                givenArray.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    
+    return givenArray;
 };
 
 // Do not edit below this line
